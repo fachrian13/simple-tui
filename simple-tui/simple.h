53c9f26b4699d00177dd4671cc8da715ec7c0e8d
Fahri <fachrian13@outlook.com>
2024-11-14 19:14:02 +0700

    COLOR

---
+++ simple-tui/simple.h
@@ -0,0 +1,26 @@
+#ifndef _SIMPLE_TUI_
+#define _SIMPLE_TUI_
+
+namespace simple {
+	enum COLOR : int {
+		BLACK = 30,
+		RED = 31,
+		GREEN = 32,
+		YELLOW = 33,
+		BLUE = 34,
+		MAGENTA = 35,
+		CYAN = 36,
+		WHITE = 37,
+		DEFAULT = 39,
+		BRIGHT_BLACK = 90,
+		BRIGHT_RED = 91,
+		BRIGHT_GREEN = 92,
+		BRIGHT_YELLOW = 93,
+		BRIGHT_BLUE = 94,
+		BRIGHT_MAGENTA = 95,
+		BRIGHT_CYAN = 96,
+		BRIGHT_WHITE = 97
+	};
+}
+
+#endif
